6aa0eddd3dbfa867d72f0aae264167be82c5198f
Kumar Ghosh <kumrzz@gmail.com>
2017-02-06 18:35:49 +0000

    Initial commit

---
+++ lib/version.py
@@ -0,0 +1,1 @@
+PYMONEROTOOLS_VERSION = '0.1' # version of the package
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import os
+import sys
+import imp
+
+version = imp.load_source('version', 'lib/version.py')
+
+if sys.version_info[:3] < (2, 7, 0):
+    sys.exit("Error: pymonerotools requires Python version >= 2.7.0...")
+
+setup(name='pymonerotools',
+      version=version.PYMONEROTOOLS_VERSION,
+      install_requires=[
+          'requests',
+      ],
+      description='monero tools in python emulating monero C++ code',
+      long_description='monero tools in python using code lifted from/emulating the monero C++ libraries',
+      keywords='monero tools python',
+      url='http://github.com/kumrzz/pymonerotools',
+      author='Kumar Ghosh',
+      author_email='bogusmailaddy@wont.work',
+      license='MIT',
+      packages=['funniest'],
+      classifiers=[
+        'Development Status :: 0.1 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+      ],
+      include_package_data=True,
+      zip_safe=False)
